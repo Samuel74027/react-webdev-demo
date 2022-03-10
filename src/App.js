6713ca90041f6ce0170533dccc83e5bbfc6cb4cc
Samuel Lee <samuel.yenlin@gmail.com>
2022-03-09 16:27:34 -0800

    week 2 updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
+import "./counter.css";
 import Counter from "./counter.jsx";
+
 function App() {
+  const [counterArray, setCounterArray] = useState([1, 2]);
+  const addCounter = () => {
+    setCounterArray([...counterArray, counterArray.length + 1]);
+  };
+  const deleteCounter = () => {
+    if (counterArray.length > 0) {
+      var temparr = counterArray;
+      temparr.pop();
+      setCounterArray([...temparr]);
+    }
+  };
+  // spread opporator
   return (
     <div>
-      <Counter />
-      <Counter />
+      {counterArray.map((elem, idx) => {
+        return <Counter />;
+      })}
+      <button onClick={addCounter}>add counter</button>
+      <button onClick={deleteCounter}>remove counter</button>
     </div>
   );
 }
--- src/counter.jsx
+++ src/counter.jsx
@@ -8,11 +8,24 @@ function Counter() {
   const decrement = () => {
     setCounter(counter - 1);
   };
+  const color = counter === 0 ? "red" : "green";
   return (
     <div>
       <h1>{counter}</h1>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
+      <button
+        style={{ BackgroundColor: "green" }}
+        className="plus-button"
+        onClick={increment}
+      >
+        +1
+      </button>
+      <button
+        style={{ BackgroundColor: "red" }}
+        className="delete-button"
+        onClick={decrement}
+      >
+        -1
+      </button>
     </div>
   );
 }
